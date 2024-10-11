69e39ab0c183ff425738a969ec7ecc2f30a38e7f
mhorowitz9 <mhorowitz9@fordham.edu>
2024-10-10 21:16:09 -0400

    created lab2

---
+++ lab2/IconIfElse.gd
@@ -0,0 +1,30 @@
+extends Sprite2D
+
+@export var health = 10
+@export var booleanVar = true
+@export var speed =7.
+# Called when the node enters the scene tree for the first time.
+func _ready() :
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("move_right"):
+		position.x += speed
+	if Input.is_action_pressed("move_left"):
+		position.x -= speed
+	if Input.is_action_pressed("move_up"):
+		position.y -= speed
+	if Input.is_action_pressed("move_down"):
+		position.y += speed
+	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		#position.x = position.x + 1.
+	
+	
+	
+	
+	#if health < 0:
+	#	hide()
+	#else:
+	#	show()
